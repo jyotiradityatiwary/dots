c34c095bd359838d890cc812d47fad2f1dd5a8dc
Jyotiraditya Tiwary <no@example.com>
2023-02-28 12:56:53 +0530

    Fixes in deploy.sh + Deleted nnn.desktop
    Archlinux package provides /usr/share/applications/nnn.desktop hence
    custom desktop file is unnecessary

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,18 @@
 [ $XDG_DATA_HOME ] || XDG_DATA_HOME="$HOME/.local/share"
 
 ## Set default wallpaper if not present
-[ -f "$XDG_DATA_HOME"/wallpapers/current] || {
+[ -f "$XDG_DATA_HOME"/wallpapers/current ] || {
+	echo "Setting default wallpaper"
 	mkdir -p "$XDG_DATA_HOME"/wallpapers
-	cp default-wallpaper "$XDG_DATA_HOME"/wallpapers/
-	ln -s "$XDG_DATA_HOME"/wallpapers/default-wallpaper "$XDG_DATA_HOME"/wallpapers/current
+	cp default-wallpaper.png "$XDG_DATA_HOME"/wallpapers/
+	ln -s "$XDG_DATA_HOME"/wallpapers/default-wallpaper.png "$XDG_DATA_HOME"/wallpapers/current
 }
 
 ## Create directory for screenshots otherwise command defined in sway config will not work
 mkdir -p $HOME/Picrures/Screenshots
 
 # Deploy dotfiles
+echo "Moving sway scripts"
 mkdir -p "$XDG_CONFIG_HOME"/sway/scripts
 cp sway/config "$XDG_CONFIG_HOME"/sway/config
 cp sway/scripts/* "$XDG_CONFIG_HOME"/sway/scripts/
@@ -25,11 +27,6 @@ cp foot/foot.ini "$XDG_CONFIG_HOME"/foot/foot.ini
 mkdir -p "$XDG_CONFIG_HOME"/waybar
 cp waybar/* "$XDG_CONFIG_HOME"/waybar/
 
-[ -e /usr/share/applications/nnn.desktop ] || [ -e /usr/local/share/applications/nnn.desktop ] || [ -e "$XDG_DATA_HOME"/applications/nnn.desktop ] || {
-	mkdir -p "$XDG_DATA_HOME"/applications
-	cp applications/nnn.desktop "$XDG_DATA_HOME"/applications/nnn.desktop
-}
-
 mkdir -p "$XDG_CONFIG_HOME"/mako
 cp mako/config "$XDG_CONFIG_HOME"/mako/config
 
@@ -38,3 +35,5 @@ cp wofi/style.css "$XDG_CONFIG_HOME"/wofi/style.css
 
 mkdir -p "$XDG_CONFIG_HOME"/lf
 cp lf/* "$XDG_CONFIG_HOME"/lf/
+
+echo "Finished"
