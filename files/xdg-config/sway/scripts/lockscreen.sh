017ca17a37db019eeab727fd01a308a709da9972
Jyotiraditya Tiwary <78427496+jyotiradityatiwary@users.noreply.github.com>
2023-08-05 21:14:29 +0530

    Lockscreen now shows number of failed attempts

--- files/xdg-config/sway/scripts/lockscreen.sh
+++ files/xdg-config/sway/scripts/lockscreen.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-[ $XDG_DATA_DIR ] || XDG_DATA_DIR="$HOME/.local/share"
 # Times the screen off and puts it to background
 swayidle \
     timeout 10 'swaymsg "output * dpms off"' \
@@ -10,6 +9,6 @@ grim -s 0.5 /dev/stdout | convert /dev/stdin -blur 0x8 /tmp/lockscreen-blur-wall
 
 # Locks the screen immediately
 # Also, pass arguments from this script to swaylock, which is needed to implement '--daemonize'
-swaylock --image /tmp/lockscreen-blur-wallpaper-tmp $@
+swaylock --show-failed-attempts --image /tmp/lockscreen-blur-wallpaper-tmp $@
 # Kills last background task so idle timer doesn't keep running
 kill %%
